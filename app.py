387b7db06f7b5a6efad1fee51a849d658f48b38f
Yaya Toure <106351938+CodingYayaToure@users.noreply.github.com>
2024-09-13 23:03:55 +0000

    Add files via upload
    Projet Certification Collecte et Fouille de Donnees  | ### Analyse des Résultats
    
    1. **Score d'entraînement** : Ce score indique comment le modèle s'adapte aux données d'entraînement. Un score proche de 1 signifie un bon ajustement, tandis qu'un score négatif indique un mauvais ajustement. Les Arbres de Décision et les Forêts Aléatoires montrent des scores d'entraînement élevés, ce qui indique qu'ils s'ajustent bien aux données d'entraînement.
    
    2. **Score de test** : Ce score indique la performance du modèle sur des données qu'il n'a pas vues. Les Forêts Aléatoires ont le meilleur score de test (0.48), suivies des Arbres de Décision et des K Plus Proches Voisins (0.40). Les autres modèles (Régression Linéaire, SVM, Réseaux Neuronaux) affichent des scores de test beaucoup plus faibles, ce qui suggère qu'ils ne généralisent pas bien.
    
    3. **RMSE de test** : L'erreur quadratique moyenne (RMSE) mesure la différence entre les valeurs prédites et les valeurs réelles. Un RMSE plus faible indique de meilleures performances. Les Forêts Aléatoires ont le RMSE le plus bas (95.35), suivies des Arbres de Décision (102.24) et des K Plus Proches Voisins (102.87). Les autres modèles ont des RMSE plus élevés, ce qui indique des prédictions moins précises.
    
    ### Conclusion
    
    **Meilleur Modèle** : 
    
    Sur la base des résultats fournis, **les Forêts Aléatoires** semblent être le meilleur modèle pour prédire le prix des produits. Elles affichent le meilleur score de test (0.48) et le RMSE le plus bas (95.35), ce qui indique qu'elles généralisent bien aux nouvelles données et fournissent des prédictions relativement précises.

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+# Charger le meilleur modèle
+best_rf = joblib.load('meilleur_modele_rf.pkl')
+
+# Informations personnelles
+#photo_url = "https://github.com/CodingYayaToure/Premier_projet/blob/main/yaya_cncs-modified%20(1).png?raw=true"
+photo_url = "CFD_2024.png"
+prenom = "Yaya"
+nom = "Toure"
+email = "yaya.toure@unchk.edu.sn"
+whatsapps_url = "https://wa.me/message/GW7RWRW3GR4WN1"
+linkedin_url = "https://www.linkedin.com/in/yaya-toure-8251a4280/"
+github_url = "https://github.com/CodingYayaToure"
+universite = "UNCHK"
+formation = "Licence Analyse Numerique et Modelisation | Master Calcul scientifique et Modelisation"
+certification = "Collecte et Fouille de Donnees (UADB-CNAM Paris)"
+responsable_cours = "Professeur Abdoulaye Barro"
+email_responsable = "abdoulayebarro9@gmail.com"
+
+# Interface Streamlit
+st.sidebar.title("Informations personnelles")
+st.sidebar.image(photo_url, caption=f"{prenom} {nom}", width=450)
+st.sidebar.write(f"Nom: {prenom} {nom}")
+st.sidebar.write(f"Email: {email}")
+st.sidebar.markdown(f"[Whatsapps]({whatsapps_url})")
+st.sidebar.markdown(f"[LinkedIn]({linkedin_url})")
+st.sidebar.markdown(f"[GitHub]({github_url})")
+st.sidebar.write(f"Université: {universite}")
+st.sidebar.write(f"**Formations:** {formation}")
+st.sidebar.write(f"**Certification:** {certification}")
+
+# Description du projet
+st.title("Prédiction de Prix de Produit")
+
+st.write("""
+Ce projet utilise des techniques d'apprentissage supervisé pour prédire le prix de produits en fonction de leurs spécifications techniques. Les variables d'entrée utilisées pour la prédiction incluent la RAM, le stockage et la taille de l'écran. 
+
+### Objectif du Projet
+
+L'objectif principal est d'obtenir la certification en collecte et fouille de données de l'UADB-CNAM de Paris. Le responsable du cours est le professeur Abdoulaye Barro, que vous pouvez contacter à l'adresse suivante : [abdoulayebarro9@gmail.com](mailto:abdoulayebarro9@gmail.com) ou par telephone : +221 77 262 80 20.
+
+### Méthodologie
+
+Nous avons développé un modèle de régression basé sur des données historiques de prix et des caractéristiques de produits. L'application permet aux utilisateurs de saisir les spécifications d'un produit et d'obtenir une estimation de son prix, facilitant ainsi la prise de décision lors de l'achat.
+
+### Variables d'Entrée
+
+Les variables d'entrée pour la prédiction sont :
+- **RAM (GB)** : La mémoire vive du produit.
+- **Storage (GB)** : La capacité de stockage du produit.
+- **Screen Size (inches)** : La taille de l'écran du produit.
+
+Cette approche permet d'utiliser efficacement les données pour générer des prédictions précises et utiles.
+""")
+
+# Entrées utilisateur pour la prédiction
+ram = st.number_input("RAM (GB)", min_value=0)
+storage = st.number_input("Stockage (GB)", min_value=0)
+screen_size = st.number_input("Taille d'écran (pouces)", min_value=0.0)
+
+# Bouton pour faire la prédiction
+if st.button("Prédire le Prix"):
+    new_data = pd.DataFrame({
+        'RAM (GB)': [ram],
+        'Storage (GB)': [storage],
+        'Screen Size (inches)': [screen_size]
+    })
+    prediction = best_rf.predict(new_data)
+    st.write(f'Prix prédit: ${prediction[0]:.2f}')
